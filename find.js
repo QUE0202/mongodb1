0ac73dd5bb00f5381cfa6721322c7dda18b47c33
que2115 <jakub1016@gmail.com>
2021-10-04 15:43:04 +0200

    MongoDB Find

---
+++ find.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://Admin:Admin12!@cluster0.p2uqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("customers").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
